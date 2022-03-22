55ca3eee478ea7fc9cc0d72baf266c915b507681
Noah Sedlik <noahsedlik@berkeley.edu>
2022-03-21 22:31:10 -0700

    add buzzer test script

--- Theremin.ino
+++ Theremin.ino
@@ -1,6 +1,16 @@
+#define BUZZER 4
 
 void setup() {
+	pinMode(BUZZER, OUTPUT);
+	/* tone(PIN, FREQ[, DURATION]) */
+	testBuzzer(BUZZER);
 }
 
 void loop() {
+}
+
+/** Plays a sound A=440hz for 1 second. Used to test that the BUZZER 
+ * configuration is correct. */
+void testBuzzer(int buzzerPin) {
+	tone(buzzerPin, 440, 1000);
 }
